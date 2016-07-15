59c96322717bde4e80309b7a90577e34799a7f9f
Zong Lei <leasingzong@gmail.com>
2016-07-16 03:09:11 +0800

    Write the homepage's UI

--- app/Home.js
+++ app/Home.js
@@ -8,71 +8,20 @@ import {
     Text,
     Image,
     Platform,
+    Dimensions,
     TouchableHighlight,
 } from 'react-native'
 
+var Screen = Dimensions.get('window');
 var GiftedListView = require('react-native-gifted-listview');
 import {fetDefaultShots, fetDebutsShots} from './api.js'
 
 export default class HomeView extends Component {
     constructor(props) {
         super(props)
-        // this.state = {
-        //     infos: []
-        // }
     }
 
     render() {
-      // let shot;
-      //
-      // fetDefaultShots(0).catch((error) => {
-      // })
-      // .then((responseData) => {
-      //   console.log('111111111');
-      //   console.log(responseData);
-      //   shot = responseData[0];
-      //   console.log(shot.user.avatar_url);
-      // }).done();
-      // let info = {
-      //   avatar: "5",
-      //   created_at: "1404784104",
-      //   id: "48",
-      //   nickname: "Nicole Sobon",
-      //   original_pic: "",
-      //   text: "Sometimes the hardest part isn't letting go, but rather, learning to start over.",
-      // };
-      //   return (
-      //     <View style={styles.container}>
-      //       <TouchableHighlight underlayColor='transparent' onPress={this._gotoDetails.bind(this, info)}>
-      //           <View style={styles.tweetContainer}>
-      //               <View style={styles.topContainer}>
-      //                   <Image source={{uri: this._getAvatarUrl(info.avatar)}} style={styles.avatar} />
-      //                   <View>
-      //                       <View style={styles.userContainer}>
-      //                           <Text style={styles.name}>{info.nickname}</Text>
-      //                           <Text style={styles.time}>{'#' + info.id + ' '} </Text>
-      //                       </View>
-      //                   </View>
-      //               </View>
-      //               <View style={styles.middleContainer}>
-      //                   <Text>{info.text}</Text>
-      //                   {this._renderMsgImage(info)}
-      //               </View>
-      //               <View style={styles.bottomContainer}>
-      //                   <TouchableHighlight style={styles.bottomTool}>
-      //                       <Text style={styles.bottomToolText}>Forward</Text>
-      //                   </TouchableHighlight>
-      //                   <TouchableHighlight style={styles.bottomTool}>
-      //                       <Text style={styles.bottomToolText}>Comment</Text>
-      //                   </TouchableHighlight>
-      //                   <TouchableHighlight style={styles.bottomTool}>
-      //                       <Text style={styles.bottomToolText}>Like</Text>
-      //                   </TouchableHighlight>
-      //               </View>
-      //           </View>
-      //       </TouchableHighlight>
-      //     </View>
-      //   )
       let color = 'gray'
         return (
             <GiftedListView
@@ -90,104 +39,49 @@ export default class HomeView extends Component {
     }
 
     _onFetch(page = 1, callback, options) {
-      let info = {
-        avatar: "5",
-        created_at: "1404784104",
-        id: "48",
-        nickname: "Nicole Sobon",
-        original_pic: "",
-        text: "Sometimes the hardest part isn't letting go, but rather, learning to start over.",
-      };
-            // if(page === 1 && options.firstLoad) {
-            //   callback(info)
-            // } else if(page === 1 && !options.firstLoad) {
-            //   callback(info)
-            // } else {
-            //   callback(info)
-            // }
       fetDefaultShots(page).catch((error) => {
       })
       .then((responseData) => {
-        console.log(responseData);
-        console.log('name == ' + responseData[0].user.name);
-        console.log('avatar_url == ' + responseData[0].user.avatar_url);
-        console.log("title == " + responseData[0].title);
-        console.log('description == ' + responseData[0].description);
-        console.log('animated == ' + responseData[0].animated);
-        console.log('normal == ' + responseData[0].images.normal);
-        console.log('teaser == ' + responseData[0].images.teaser);
-        console.log('views_count == ' + responseData[0].views_count);
-        console.log('comment_count == ' + responseData[0].comment_count);
-        console.log('likes_count == ' + responseData[0].likes_count);
+        callback(responseData)
       }).done();
-
-      if(page === 1 && options.firstLoad) {
-        callback([info, info, info, info, info])
-      } else if (page === 1 && !options.firstLoad) {
-        callback([info, info, info, info, info])
-      } else {
-        callback([info, info, info, info, info], {
-                        allLoaded: true
-                    })
-      }
-      // if (page === 3) {
-      //   callback(info, {allLoaded: true})
-      // } else {
-      //   console.log('refresh');
-      //   fetDebutsShots(page).catch((error) => {
-      //   })
-      //   .then((responseData) => {
-      //     console.log(responseData);
-      //     console.log(page);
-      //     callback(info, {allLoaded: false});
-      //   }).done();
-      // }
    }
 
-   _renderRowView(info) {
-        return (
-                <TouchableHighlight underlayColor='transparent' onPress={this._gotoDetails.bind(this, info)}>
-                    <View style={styles.tweetContainer}>
-                        <View style={styles.topContainer}>
-                            <Image source={{uri: this._getAvatarUrl(info.avatar)}} style={styles.avatar} />
-                            <View>
-                                <View style={styles.userContainer}>
-                                    <Text style={styles.name}>{info.nickname}</Text>
-                                    <Text style={styles.time}>{'#' + info.id + ' '} </Text>
-                                </View>
-                            </View>
-                        </View>
-                        <View style={styles.middleContainer}>
-                            <Text>{info.text}</Text>
-                            {this._renderMsgImage(info)}
-                        </View>
-                        <View style={styles.bottomContainer}>
-                            <TouchableHighlight style={styles.bottomTool}>
-                                <Text style={styles.bottomToolText}>Forward</Text>
-                            </TouchableHighlight>
-                            <TouchableHighlight style={styles.bottomTool}>
-                                <Text style={styles.bottomToolText}>Comment</Text>
-                            </TouchableHighlight>
-                            <TouchableHighlight style={styles.bottomTool}>
-                                <Text style={styles.bottomToolText}>Like</Text>
-                            </TouchableHighlight>
-                        </View>
-                    </View>
-                </TouchableHighlight>
-        )
-    }
-
-    _renderMsgImage(info) {
-            if(info.original_pic) {
-                return (
-                    <Image source={{uri: info.original_pic}} style={[styles.msgImage, { resizeMode: Image.resizeMode.cover }]} />
-                )
-            }
-        }
-
-    _getAvatarUrl(ID) {
-        return 'http://lorempixel.com/68/68/people/' + ID
-    }
+    _renderRowView(info) {
+         return (
+                 <TouchableHighlight underlayColor='transparent' onPress={this._gotoDetails.bind(this, info)}>
+                     <View style={styles.tweetContainer}>
+                         <View style={styles.topContainer}>
+                             <Image source={{uri: info.user.avatar_url}}
+                             style={styles.avatar} />
+                             <View style={{justifyContent: 'center'}}>
+                                 <Text style={styles.name}>{info.user.name}</Text>
+                             </View>
+                         </View>
+                         <View style={styles.middleContainer}>
+                             <Image source={{uri: info.images.normal}}
+                             style={{height: info.height}}/>
+                         </View>
+                         <View style={styles.bottomContainer}>
+                                 <View style={[styles.bottomCell, styles.bottomTool]}>
+                                     <Image style={{resizeMode: 'cover'}}
+                                     source={require('../img/visibility_grey.png')} />
+                                     <Text style={styles.bottomToolText}>{' ' + info.views_count}</Text>
+                                 </View>
+                                 <View style={[styles.bottomCell, styles.bottomTool]}>
+                                     <Image style={{resizeMode: 'cover'}}
+                                     source={require('../img/message_grey.png')} />
+                                     <Text style={styles.bottomToolText}>{' ' + info.comments_count}</Text>
+                                 </View>
+                                 <View style={[styles.bottomCell, styles.bottomTool]}>
+                                     <Image style={{resizeMode: 'cover'}}
+                                     source={require('../img/favorite_grey.png')} />
+                                     <Text style={styles.bottomToolText}>{' ' + info.likes_count}</Text>
+                                 </View>
+                         </View>
+                     </View>
+                 </TouchableHighlight>
+         )
+     }
 
     _gotoDetails(tweet) {
       console.log('goto detail');
@@ -204,33 +98,19 @@ const listStyles = {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
   tweetContainer: {
     backgroundColor: 'white',
-    margin: 10,
-    marginBottom: 0,
-    borderRadius: 2,
+    marginTop: 3,
+    marginBottom: 7,
+    marginRight: 5,
+    marginLeft: 5,
+    borderRadius: 5,
     shadowColor: '#000000',
     shadowOpacity: 0.1,
     shadowRadius: 0,
     shadowOffset: {
     height: 2,
-    width: 1
+    width: 1,
     }
   },
   topContainer: {
@@ -238,35 +118,25 @@ const styles = StyleSheet.create({
       flexDirection: 'row',
       padding: 10,
       borderColor: '#e1e1e1',
-      borderBottomWidth: 1
+      borderBottomWidth: 1,
+      alignItems: 'center'
   },
   avatar: {
       backgroundColor: 'gray',
-      width: 35,
-      height: 35,
-      borderRadius: 4,
+      width: 36,
+      height: 36,
+      borderRadius: 18,
       marginRight: 6
-  },
-  time: {
-      fontSize: 13,
-      color: '#8999a5',
-      marginTop: 2
   },
   name: {
       color: '#ff9630',
       fontWeight: '600',
-      fontSize: 14
+      fontSize: 14,
   },
   middleContainer: {
       padding: 10,
       borderColor: '#e1e1e1',
       borderBottomWidth: 1
-  },
-  msgImage: {
-      flex: 1,
-      marginTop: 10,
-      height: 200,
-      backgroundColor: '#e1e1e1'
   },
   bottomContainer: {
       flexDirection: 'row',
@@ -277,6 +147,11 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center'
+  },
+  bottomCell: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
   },
   bottomToolText: {
       color: '#6D6D78',
